b461576bde46c517c6f2ce02a5c1d576d3cbdff0
Heatblazer412 <54789242+Heatblazer412@users.noreply.github.com>
2026-08-16 12:50:58 -0500

    Final Commit

--- src/CreateDataset.py
+++ src/CreateDataset.py
@@ -8,11 +8,14 @@ Created on Fri Jun 19 16:14:25 2026
 import numpy as np
 from Preprocessing import build_dataset
 from Preprocessing import DATA_PATH
+from Preprocessing import VALID_RUNS
+
 import os
 
 X, y = build_dataset()
 
 np.save(os.path.join(DATA_PATH, "X.npy"), X)
 np.save(os.path.join(DATA_PATH, "y.npy"), y)
+np.save(os.path.join(DATA_PATH, "valid_runs.npy"), np.array(VALID_RUNS))
 
 print("Dataset saved.")
